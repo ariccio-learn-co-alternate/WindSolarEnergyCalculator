d82c558adb49f1d9b00774757dce5e13088951c3
Tori Sigler <mssigler@gmail.com>
2019-07-10 17:26:54 -0400

    Added logic

--- wind-solar-energy-calculator/app/controllers/locations_controller.rb
+++ wind-solar-energy-calculator/app/controllers/locations_controller.rb
@@ -1,9 +1,8 @@
 class LocationsController < ApplicationController
     before_action :authorized?
-
     
     def index
-        @user = session[:user_id] 
+        @user = session[:user_id]
         @states 
         @states = WindValue.all.map do |w|
             w.state
@@ -19,7 +18,6 @@ class LocationsController < ApplicationController
  
         @user = session[:user_id]
         @state = params[:state] 
-        
         @wind_city_options = WindValue.all
         Location.new(state: @state, solar_id: nil, wind_id: nil)
     end
@@ -30,6 +28,19 @@ class LocationsController < ApplicationController
         @city = params[:city]
         stateobj = WindValue.find_by(city: @city)  # go into Location table
         @state = stateobj.state
+        @speed = stateobj.speed
+
+        @windiest_cities=
+            WindValue.all.sort_by do |c|
+                c.speed
+            end
+        @annual_wind_energy = annual_turbine_energy
+    end
+
+    def annual_turbine_energy
+        @rotor_diam_ft = 18
+        
+        return (0.01328 * (@rotor_diam_ft**2) * (@speed**3) )
     end
 
 
@@ -43,6 +54,7 @@ class LocationsController < ApplicationController
     def search_params
         params.require(:state).permit(:state)
     end
+
     def results_params
         params.require(:city).permit(:city)
     end
